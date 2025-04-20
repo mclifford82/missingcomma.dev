909d95eb0d5df8304618e28a0ba0ae4f25713d46
Martin Clifford <mclifford82@gmail.com>
2025-04-20 10:22:46 -0700

    Initial Gatsby Site

--- src/components/bio.js
+++ src/components/bio.js
@@ -46,8 +46,8 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          <a href={`https://bsky.com/profile/${social?.bluesky || ``}`}>
+            You should follow them on Bluesky
           </a>
         </p>
       )}
